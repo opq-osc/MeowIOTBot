511f705b5e88161b0ad946cde492cb3de93df477
Tennant David <DavidTennant405@outlook.com>
2021-05-25 15:40:56 +0800

    update all to 5.0.0

--- obj/Debug/net5.0/MeowIOTBot.AssemblyInfo.cs
+++ obj/Debug/net5.0/MeowIOTBot.AssemblyInfo.cs
@@ -14,14 +14,14 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Electronicute")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2020 @ Electronicute")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("5.0.0\r\n通过和框架作者了解得知最新框架不存在qq监听,将返回所有qq,请开发者注意变更参数,\r\n取消原来的connect,使用新的connect参数;\r\n框" +
-    "架优化ping - pong check 增加了客户端Log记录类型\r\n框架优化操作函数,修复了群撤回信息读取不正常的错误\r\n框架优化操作参量,Json/XML" +
-    "信息解析优化")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("4.3.2.2")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("var 5.0.0\r\n====详细操作请看wiki更新====\r\n通过和框架作者了解得知最新框架不存在qq监听,将返回所有qq,请开发者注意变更参数,\r\n取消原来" +
+    "的connect,使用新的connect参数;\r\n框架优化ping - pong check 增加了客户端Log记录类型\r\n框架优化操作函数,修复了群撤回信息读" +
+    "取不正常的错误\r\n框架优化操作参量,Json/XML信息解析优化")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("5.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("5.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeowIOTBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeowIOTBot")]
-[assembly: System.Reflection.AssemblyVersionAttribute("4.3.2.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("5.0.0.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/DavidSciMeow/MeowIOTBot")]
 [assembly: System.Resources.NeutralResourcesLanguageAttribute("zh")]
 
